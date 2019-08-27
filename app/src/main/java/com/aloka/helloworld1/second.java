cc9e3a57f35398e931847d538abe5efc9037de13
M H Aloka <mhaloka@Ms-MacBook-Air.local>
2019-08-27 17:38:48 +0530

    Initial commit

---
+++ app/src/main/java/com/aloka/helloworld1/second.java
@@ -0,0 +1,12 @@
+package com.aloka.helloworld1;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class second extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second);
+    }
+}
